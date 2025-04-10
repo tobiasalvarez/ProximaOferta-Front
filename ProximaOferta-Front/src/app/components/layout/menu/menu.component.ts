7d2d0b2556061b91a49e1542e4792520257c03ee
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2025-04-10 19:55:12 -0300

    .

--- ProximaOferta-Front/src/app/components/layout/menu/menu.component.ts
+++ ProximaOferta-Front/src/app/components/layout/menu/menu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MdbCollapseModule],
+  imports: [MdbCollapseModule, RouterModule, RouterOutlet],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
--- ProximaOferta-Front/src/app/components/supermercado/supermercado-list/supermercado-list.component.ts
+++ ProximaOferta-Front/src/app/components/supermercado/supermercado-list/supermercado-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Supermercado } from '../../../models/supermercado';
+import { Usuario } from '../../../models/usuario';
+
 
 @Component({
   selector: 'app-supermercado-list',
@@ -8,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrl: './supermercado-list.component.scss'
 })
 export class SupermercadoListComponent {
+  lista: Supermercado[] = [];
+
+  constructor() {
+    this.lista = [
+      { id: 1, nome: 'Supermercado A', rua: 'Calle 123', email: 'abc@gmail.com', cadastroCompleto: true , usuario: new Usuario{id: 1, usuario: 'admin', senha: 'admin'}}, 
+      { id: 2, nome: 'Supermercado B', rua: 'Calle 456', email: 'abcd@gmail.com', latitud: 10.654321, longitud: -74.654321 },
+      { id: 3, nome: 'Supermercado C', rua: 'Calle 789', email: 'abcd@gmail.com', latitud: 10.789012, longitud: -74.789012 }
+    ];
+  }
+
 
 }
