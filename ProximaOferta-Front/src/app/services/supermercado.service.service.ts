02532fc0c980202f27655996e6564a31f82bfc92
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2025-04-11 21:17:15 -0300

    service

--- ProximaOferta-Front/src/app/services/supermercado.service.service.ts
+++ ProximaOferta-Front/src/app/services/supermercado.service.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@angular/core';
-
+import { inject, Injectable } from '@angular/core';
+import { Supermercado } from '../models/supermercado';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class SupermercadoServiceService {
 
-  
+  http = inject(HttpClient);
+  API = 'http://localhost:8080/api/supermercado';
 
   constructor() { }
+
+  findAll():Observable<Supermercado[]>{
+    return this.http.get<Supermercado[]>(this.API+'/findAll');
+  }
+
+  findById(id: number): Observable<Supermercado> {
+    return this.http.get<Supermercado>(this.API + '/findById/' + id);
+  }
+
+  
+
+
+
 }
