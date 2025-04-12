ef6226788ce866586799624b4806a566497fc795
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2025-04-11 21:58:04 -0300

    supermercado service

--- ProximaOferta-Front/src/app/components/supermercado/supermercado-list/supermercado-list.component.ts
+++ ProximaOferta-Front/src/app/components/supermercado/supermercado-list/supermercado-list.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Supermercado } from '../../../models/supermercado';
 import { Usuario } from '../../../models/usuario';
-
+import { SupermercadoServiceService } from '../../../services/supermercado.service.service';
+import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-supermercado-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './supermercado-list.component.html',
   styleUrl: './supermercado-list.component.scss'
 })
 export class SupermercadoListComponent {
   lista: Supermercado[] = [];
+  supermercadoService = inject(SupermercadoServiceService);
 
   constructor() {
-    this.lista = [
-      { id: 1, nome: 'Supermercado A', rua: 'Calle 123', email: 'abc@gmail.com', cadastroCompleto: true , usuario: new Usuario{id: 1, usuario: 'admin', senha: 'admin'}}, 
-      { id: 2, nome: 'Supermercado B', rua: 'Calle 456', email: 'abcd@gmail.com', latitud: 10.654321, longitud: -74.654321 },
-      { id: 3, nome: 'Supermercado C', rua: 'Calle 789', email: 'abcd@gmail.com', latitud: 10.789012, longitud: -74.789012 }
-    ];
+   this.findAll();
   }
 
-
+  findAll(){
+   
+    this.supermercadoService.findAll().subscribe({
+      next: (listaRetornada) => {
+        this.lista = listaRetornada;
+      },
+      error: (erro) => {
+        Swal.fire(erro.error, '', 'error');
+      }
+    });
+  
+  }
 }
--- ProximaOferta-Front/src/app/services/supermercado.service.service.ts
+++ ProximaOferta-Front/src/app/services/supermercado.service.service.ts
@@ -20,7 +20,17 @@ export class SupermercadoServiceService {
     return this.http.get<Supermercado>(this.API + '/findById/' + id);
   }
 
-  
+  deleteById(id: number): Observable<string> {
+    return this.http.delete<string>(this.API + '/deleteById/' + id, {responseType : 'text' as 'json'});
+  }
+
+  save(supermercado: Supermercado): Observable<string>{
+    return this.http.post<string>(this.API + '/save', supermercado, {responseType : 'text' as 'json'});
+  }
+
+  update(supermercado :Supermercado, id: number): Observable<string>{
+    return this.http.put<string>(this.API+'/update/' + id, supermercado, {responseType: 'text' as 'json'});
+  }
 
 
 
